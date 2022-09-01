bbf19f0034406a57751128576ec8d7aee4d60625
Jack Bowman <jbbowman@students.unwsp.edu>
2022-09-01 18:20:54 -0500

    Created 5 students and calculated average height

---
+++ Ch2a_1.java
@@ -0,0 +1,14 @@
+public class Ch2a_1 {
+
+    public static void main(String[] args) {
+        double student1 = 80;
+        double student2 = 100;
+        double student3 = 120;
+        double student4 = 90;
+        double student5 = 140;
+
+        double average = (student1 + student2 + student3 + student4 + student5) / 5;
+
+        System.out.println("The average height is " + average);
+    }
+}
