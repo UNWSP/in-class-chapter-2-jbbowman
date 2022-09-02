9e3db17fefabe616b1b377ab45722af582f12db9
Jack Bowman <jbbowman@students.unwsp.edu>
2022-09-01 19:10:10 -0500

    Created 5 students and calculated average height

---
+++ Ch2a_2.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class Ch2a_2 {
+
+    public static void main(String[] args) {
+        int ticketAmount;
+        final float TICKET = 75, FEE = 20;
+        final double TAX = 1.07, total;
+        String input;
+
+        input = JOptionPane.showInputDialog(null, "Enter number of tickets: ");
+        ticketAmount = Integer.parseInt(input);
+        total = ((ticketAmount * TICKET) + FEE) * TAX;
+
+        JOptionPane.showMessageDialog(null, String.format("Your total is $%,.2f", total));
+
+    }
+}
